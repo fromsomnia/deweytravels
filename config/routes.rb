9e2db2752970745777d911627f4a96c7f026dce9
William Chidyausiku <wchid@stanford.edu>
2014-02-20 07:43:49 -0800

    fixed routing issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 TeamDeweyWebsite::Application.routes.draw do
-  get "users/index"
+  get "users"
   resources :graphs
 
   resources :topics
@@ -12,7 +12,7 @@ TeamDeweyWebsite::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#index'
+  # root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
