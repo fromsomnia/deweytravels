2428b0bf6075a7f4b443aba24b4985ed36fcfc52
William Chidyausiku <wchid@stanford.edu>
2014-02-27 19:42:43 -0800

    added chronological sorting of events

---
+++ db/migrate/20140228021433_create_topics_users_join.rb
@@ -0,0 +1,8 @@
+class CreateTopicsUsersJoin < ActiveRecord::Migration
+  def change
+    create_table :topics_users, :id => false do |t|
+    	t.column :expertise_id, :integer
+    	t.column :expert_id, :integer
+    end
+  end
+end
