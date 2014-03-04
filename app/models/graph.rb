0a0faf1f77aacf226f6c4629776e617e2330d968
William Chidyausiku <wchid@stanford.edu>
2014-03-04 06:08:27 -0800

    added functionality to get graph experts, as well as graph printout validation

--- app/models/graph.rb
+++ app/models/graph.rb
@@ -3,4 +3,16 @@ class Graph < ActiveRecord::Base
 
 	has_and_belongs_to_many :topics
 
+	def experts
+		@experts = []
+		self.topics.each do |topic|
+			topic.experts.each do |expert|
+				if !@experts.include?(expert) then
+					@experts << expert
+				end
+			end
+		end
+		return @experts
+	end
+
 end
--- test/models/graph_test.rb
+++ test/models/graph_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class GraphTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "all graphs" do
+  	puts "Displaying All Graph Information..."
+  	puts ""
+  	Graph.all.each do |graph|
+  		puts "GRAPH =================" + graph.title + "=================="
+  		puts "    Graph Title: " + graph.title
+  		puts ""
+
+  		puts "    Experts Involved: (" + graph.experts.size.to_s + ")"
+  		graph.experts do |expert|
+  			puts "        " + expert.first_name + " " + expert.last_name + " - Skill: " + expert.expertises.size.to_s
+  		end
+  		puts ""
+
+  		puts "    Constituent Topics: (" + graph.topics.size.to_s + ")"
+  		graph.topics.each do |topic|
+  			puts "        " + topic.title + " - Experts: " + topic.experts.size.to_s
+  		end
+  		puts "======================================="
+  		puts ""
+  		puts ""
+  	end
+    assert true
+  end
 end
