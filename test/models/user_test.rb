8cbf24a98ca2a452beb25f1fe712fa92df20d0d8
William Chidyausiku <wchid@stanford.edu>
2014-03-04 03:51:23 -0800

    added simple printout confirmation of user relations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,18 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-  	william = users(:william)
-  	puts william.first_name
-  	puts "Skill Set"
-  	william.expertises.each do |expertise|
-  		puts ""
-  		puts expertise.title
-  		puts "subtopics"
-  		expertise.subtopics.each do |l|
-  			puts l.title
-  		end
+  test "all users" do
+  	puts "Displaying All User Information..."
+  	puts ""
+  	User.all.each do |user|
+  		puts user.first_name + ":"
 
+  		puts " First Name: " + user.first_name
+  		puts " Last Name: " + user.last_name
+  		puts " Email: " + user.email
+  		puts " Username: " + user.username
+  		puts " Password: " + user.password
+  		puts " Position: " + user.position
+  		puts " Department: " + user.department
+  		puts " Image: " + user.image
+
+  		puts ""
+  		puts " " + user.first_name + "'s Expertise:"
+  		user.expertises.each do |expertise|
+  			puts "    " + expertise.title
+  		end
+  		puts ""
+  		puts ""
   	end
   	assert true
   end
