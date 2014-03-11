e929969da566b810fc750a8d8106e8aa4a4b4346
William Chidyausiku <wchid@stanford.edu>
2014-03-11 06:23:58 -0700

    fixed migration error

--- db/migrate/20140311131042_populate_user_hierarchy.rb
+++ db/migrate/20140311131042_populate_user_hierarchy.rb
@@ -1,30 +1,30 @@
 class PopulateUserHierarchy < ActiveRecord::Migration
   def change
-	user = User.find_by_first_name("chris")
-	 user.superiors << User.find_by_first_name("jay")
+	user = User.find_by_first_name("Chris")
+	 user.superiors << User.find_by_first_name("Jay")
 
-	user = User.find_by_first_name("oren")
-	 user.superiors << User.find_by_first_name("jay")
+	user = User.find_by_first_name("Oren")
+	 user.superiors << User.find_by_first_name("Jay")
 
-	user = User.find_by_first_name("marc")
-	 user.superiors << User.find_by_first_name("oren")
+	user = User.find_by_first_name("Marc")
+	 user.superiors << User.find_by_first_name("Oren")
 
-	user = User.find_by_first_name("oren")
-	 user.subordinates << User.find_by_first_name("mike")
+	user = User.find_by_first_name("Oren")
+	 user.subordinates << User.find_by_first_name("Mike")
 
-	user = User.find_by_first_name("jay")
-	 user.subordinates << User.find_by_first_name("william")
+	user = User.find_by_first_name("Jay")
+	 user.subordinates << User.find_by_first_name("William")
 
-	user = User.find_by_first_name("jay")
-	 user.subordinates << User.find_by_first_name("veni")
+	user = User.find_by_first_name("Jay")
+	 user.subordinates << User.find_by_first_name("Veni")
 
-	user = User.find_by_first_name("jay")
-	 user.subordinates << User.find_by_first_name("john")
+	user = User.find_by_first_name("Jay")
+	 user.subordinates << User.find_by_first_name("John")
 
-	user = User.find_by_first_name("jay")
-	 user.subordinates << User.find_by_first_name("brett")
+	user = User.find_by_first_name("Jay")
+	 user.subordinates << User.find_by_first_name("Brett")
 
-	user = User.find_by_first_name("jay")
-	 user.subordinates << User.find_by_first_name("stephen")
+	user = User.find_by_first_name("Jay")
+	 user.subordinates << User.find_by_first_name("Stephen")
   end
 end
