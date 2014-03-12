9c59e3db33c90fe2ba076ee3d014db4def3902d1
Veni Johanna <angelina.veni@gmail.com>
2014-03-12 00:25:54 -0700

    Integrate login form with login backend

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,21 +5,23 @@ class SessionController < ApplicationController
   def login
   end
 
-
   def post_login
     email = params[:email]
     password = params[:password]
     sc = Socialcast.new(email, password)
     status = sc.authenticate
+
     if status['authentication-failure']
       err = status['authentication-failure']
       render json: err, status: :unauthorized
     else
-      #TODO: figure out what to do if login belongs to > 1 community
+      # TODO: figure out what to do if login belongs to > 1 community
       info = status['communities'][0]
       domain = info['subdomain']
       user_id = info['profile']['id']
-      #if company is not in db, fetch all employees:
+
+      # TODO: keep track of user's password too - for bg processes
+      # If company is not in db, fetch all employees:
       if !User.exists?(domain: domain)
         puts "fetching all users"
         User.load_from_sc(sc, domain)
