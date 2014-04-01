f225c3a72dab583d59d33bd676a1ea8600e39d91
Veni Johanna <angelina.veni@gmail.com>
2014-04-01 08:27:22 -0700

    add library to access freebase.
    adapted from https://github.com/tombenner/freeb/, main changes are in topic.rb and API.rb. The changes are to make this work as standalone (not as gem) and to with our version of ruby. Big feature we are missing right now is the ability to cache results.
    
    note: only the search function is tested. currently only using this function.

---
+++ lib/freeb/freeb_topic.rb
@@ -0,0 +1,72 @@
+module Freeb
+  class FreebTopic
+    @raw_data = nil
+
+    def initialize(id_or_hash)
+      if id_or_hash.is_a?(String)
+        response = API.get(id)
+      else
+        response = id_or_hash
+      end
+      init_with_response(response)
+      return nil if @raw_data.blank?
+      self
+    end
+
+    def init_with_response(response)
+      @raw_data = response
+    end
+
+    def description
+      @raw_data["description"] ||= API.description(id)
+    end
+
+    def raw_data
+      @raw_data
+    end
+
+    def id
+      get_property("mid")
+    end
+
+    def name
+      get_property("name")
+    end
+
+    def image_url
+      "https://usercontent.googleapis.com/freebase/v1/image#{self.id}?maxwidth=225&maxheight=225&mode=fillcropmid"
+    end
+
+    def get_property(property)
+      property = property.to_s
+      return nil if property == "instance"
+      return nil if @raw_data.blank?
+      return description if property == "description"
+      return @raw_data[property] unless @raw_data[property].blank?
+
+      unless @raw_data['type'].blank?
+        id = "#{@raw_data['type']}/#{property}"
+        return @raw_data[id] unless @raw_data[id].blank?
+      end
+
+      mql = {
+        "id" => get_property(:id),
+        property => nil
+      }
+
+      result = API.mqlread(mql)
+      @raw_data[property] = result[property]
+      return @raw_data[property]
+    end
+
+    def [](key)
+      get_property(key)
+    end
+
+    private
+
+    def method_missing(method, *args)
+      get_property(method)
+    end
+  end
+end
